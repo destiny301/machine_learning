74d43c6f15611ae5c060118860066f7756d8958d
Your Name <you@example.com>
2020-08-07 03:54:26 -0700

    hw01_v2

--- week01/hw01_gridCV.py
+++ week01/hw01_gridCV.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras import Model
+from tensorflow.keras import regularizers
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 from sklearn.model_selection import GridSearchCV
@@ -13,10 +14,10 @@ pixels = 28*28
 xtr = xtr.reshape((60000, pixels)).astype(np.float32) / 255.0
 xte = xte.reshape((10000, pixels)).astype(np.float32) / 255.0
 
-def create_model(hidden_nodes, dropout):
+def create_model(hidden_nodes, dropout_rate, reg_val):
     inputs = Input(shape = (pixels,), name = 'images')
-    z = Dense(hidden_nodes, activation='relu', name = 'hidden1')(inputs)
-    z = Dropout(dropout)(z)
+    z = Dense(hidden_nodes, activation='relu', kernel_regularizer=regularizers.l2(reg_val), bias_regularizer=regularizers.l2(reg_val), name = 'hidden1')(inputs)
+    z = Dropout(dropout_rate)(z)
     z = Dense(10, activation='softmax')(z)
 
     our_model = Model(inputs = inputs, outputs = z)
@@ -25,10 +26,12 @@ def create_model(hidden_nodes, dropout):
     our_model.compile(optimizer = 'adam', loss = SparseCategoricalCrossentropy(), metrics = ['accuracy'])
     return our_model
 
-dropout = [0.2, 0.3, 0.4, 0.5]
+dropout_rate = [0.2, 0.3, 0.4, 0.5]
 hidden_nodes = [16, 32, 64]
+reg_val = [1e-3, 1e-4]
+epochs = [1, 10]
 
-params = {'dropout': dropout, 'hidden_nodes':hidden_nodes}
+params = {'hidden_nodes':hidden_nodes, 'dropout_rate': dropout_rate, 'reg_val': reg_val, 'epochs': epochs}
 model = KerasClassifier(build_fn=create_model, epochs = 10, batch_size = 32, verbose = 0)
 
 gridCV = GridSearchCV(model, params, cv = 5, n_jobs = -1)
